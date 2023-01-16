c0d646df4f0d5fd6d36ed4eb89bdfe0183bdd901
DuarteFranca <duartefranca_@outlook.com>
2023-01-16 17:12:24 +0000

    adds day1 file

---
+++ day1.js
@@ -0,0 +1,20 @@
+
+
+/* let count=0;
+while (count < 5) {
+    count++;
+    
+    if (count ===3){
+        continue;
+    }
+    console.log(count);
+} 
+ */
+
+let ourCampus = 'Onions';
+for (const value of ourcampus) {
+    console.log(value);
+}
+for (let i = 0; i < ourCampus.length; i ++) {
+    console.log(ourCampus[i]);
+}
